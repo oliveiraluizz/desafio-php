aa5a99fcb5c0a493d8b63f00f6148cc062863872
oliveiraluizz <oliv.luizreis@gmail.com>
2021-07-30 01:39:11 -0300

    primeiro commit

---
+++ app/Models/produto.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class produto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'codigo',
+        'categoria',
+        'nome',
+        'preco',
+        'compisicao',
+        'dataCadastro',
+        'tamanho',
+        'qtdProduto'
+
+    ];
+
+}
---
+++ database/migrations/2021_07_30_032306_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->codigo();
+            $table->categoria();
+            $table->nome();
+            $table->preco();
+            $table->composicao();
+            $table->dataCadastro();
+            $table->tamanho();
+            $table->qtdProduto();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
